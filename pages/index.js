d57f1eb6f74f5a55b816b6f6de8e7fa969bd5802
Brian Kennedy <bryeguy527@gmail.com>
2021-11-15 21:57:01 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  Heading,
+  Container
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../src/ColorModeSwitcher';
+import "@fontsource/abhaya-libre"
+import { extendTheme } from "@chakra-ui/react"
+import { LogoGithubIcon } from 'chakra-ui-ionicons';
+import Background from '../src/Background';
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Abhaya Libre",
+  },
+})
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Background />
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={2}>
+            <Heading>DaddyImPregnant</Heading>
+            <Link href="https://github.com/DaddyImPregnant"><LogoGithubIcon /></Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
