9f5c041965f0385e9bdb289442b9102e32a87a9e
Brian Kennedy <bryeguy527@gmail.com>
2021-11-17 15:37:06 -0500

    Updated .drone.yml and Dockerfile

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import { render } from './test-utils';
 import App from '../pages/index.js';
 
-test('renders learn react link', () => {
+test('renders github link', () => {
   render(<App />);
   const linkElement = screen.getByText(/DaddyImPregnant/i);
   expect(linkElement).toBeInTheDocument();
