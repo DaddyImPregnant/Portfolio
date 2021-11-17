63a6f0a26d483f4cab8f16916cf7294a87d1c818
Brian Kennedy <bryeguy527@gmail.com>
2021-11-17 15:04:24 -0500

    Updated Dockerfile and Head

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ import "@fontsource/abhaya-libre"
 import { extendTheme } from "@chakra-ui/react"
 import { LogoGithubIcon } from 'chakra-ui-ionicons';
 import Background from '../src/Background';
+import Head from 'next/head'
 
 const theme = extendTheme({
   fonts: {
@@ -25,17 +26,27 @@ const theme = extendTheme({
 function App() {
   return (
     <ChakraProvider theme={theme}>
+
+      <Head>
+        <title>Brian | DaddyImPregnant</title>
+      </Head>
+
       <Background />
+
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
+
           <ColorModeSwitcher justifySelf="flex-end" />
+
           <VStack spacing={2}>
             <Heading>DaddyImPregnant</Heading>
             <Text>Software Developer</Text>
             <Link href="https://github.com/DaddyImPregnant"><LogoGithubIcon /></Link>
           </VStack>
+
         </Grid>
       </Box>
+
     </ChakraProvider>
   );
 }
